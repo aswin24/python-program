b92e53bc6c1e827ea42ed66a0b6ef698d2d02a1c
aswin24 <ashwin97.ak@gmail.com>
2018-02-23 09:59:52 +0530

    Create oddoreven.py

---
+++ oddoreven.py
@@ -0,0 +1,7 @@
+def evenodd():
+    n = input("Enter the number")
+    if int(n) % 2 == 0:
+        print("Number", n,"is even")
+    elif int(n) % 2 == 1:
+        print("Number", n,"is odd")
+evenodd()
