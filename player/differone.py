6ffa88cf344a0ff14d3b2ab73d4d1acea55af20a
Ashwin kumar <ashwin97.ak@gmail.com>
2018-07-23 14:09:47 +0530

    Update differone.py

--- player/differone.py
+++ player/differone.py
@@ -1,6 +1,6 @@
 a=raw_input("Enter the value")
 b=raw_input("ENter the value")
-if a[0]==b[0] and a[1]==b[1] and a[2]==b[2]:
+if a[0]==b[0] or a[1]==b[1] or a[2]==b[2]:
   print("no")
 else:
   print ("yes")
